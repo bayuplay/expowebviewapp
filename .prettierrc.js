75a64b6d35b1b6170fa2f0ca9e7f268b4647df95
bayuplay <bayumncplay@gmail.com>
2025-04-03 19:55:26 +0700

    Create .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// .prettierrc.js
+// File ini bisa kosong jika Anda setuju dengan pengaturan default Prettier.
+// Atau, Anda bisa menambahkan konfigurasi kustom di sini.
+module.exports = {
+  // Contoh beberapa opsi konfigurasi umum:
+  // semi: true, // Gunakan semicolon di akhir statement (default: true)
+  // singleQuote: true, // Gunakan kutip tunggal daripada kutip ganda (default: false)
+  // trailingComma: 'es5', // Tambahkan koma di akhir array/object (default: 'es5')
+  // printWidth: 80, // Batas panjang baris (default: 80)
+  // tabWidth: 2, // Jumlah spasi per tab (default: 2)
+};
