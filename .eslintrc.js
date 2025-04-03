430c56ebccf5851a3d91052eb2ca18b5c8f9b60a
bayuplay <bayumncplay@gmail.com>
2025-04-03 19:54:39 +0700

    Create .eslintrc.js
    untuk deteksi otomatis ketika ada kesalahan dalam kode yang berpotensi bikin error ini untuk js dan ts

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+// .eslintrc.js
+module.exports = {
+  // Menggunakan konfigurasi dasar yang direkomendasikan oleh Expo
+  extends: 'expo',
+  // Anda bisa menambahkan aturan ('rules') atau plugin tambahan di sini jika diperlukan
+  // rules: {
+  //   // Contoh: mewajibkan penggunaan semicolon
+  //   // semi: ['error', 'always'],
+  // },
+  // env: {
+  //   // Menentukan environment, misalnya 'jest: true' jika menggunakan Jest untuk testing
+  // },
+  // globals: {
+  //   // Mendefinisikan variabel global jika ada
+  // }
+};
